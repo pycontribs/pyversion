94cd773ecafa390d537e2d1c58b98b311e3cac1b
Kyle Rockman <kyle.rockman@mac.com>
2015-04-01 11:33:12 -0500

    fixup dependancies
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

--- setup.py
+++ setup.py
@@ -3,10 +3,7 @@ from setuptools import setup
 
 setup(
     setup_requires=[
-        'packaging==15.0',
         'pbr==0.10.7',
-        'pyversion',
     ],
     pbr=True,
-    auto_version="PBR",
 )
