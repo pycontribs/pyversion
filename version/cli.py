7964295bd339e24f99bd045bdd3e440debc82336
Kyle Rockman <kyle.rockman@mac.com>
2015-07-07 15:04:41 -0500

    add a cli command that inspect package versions
    this cli command should be used in tools where finding out the version number of a package needs to happen prior to `python setup.py` invocation
    
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

---
+++ version/cli.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from version import VersionUtils
+
+def main():
+    args = sys.argv[1:]
+    version = VersionUtils.get_version(args[0])
+    if len(args) > 1 and args[1] == 'increment':
+        print VersionUtils.increment(version)
+    else:
+        print version
+
+if __name__ == "__main__":
+    main()
